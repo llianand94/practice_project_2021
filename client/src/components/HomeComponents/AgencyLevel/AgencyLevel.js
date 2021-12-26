2a6d4bf2cc8bd42070212a9364e56f2a0915f4cc
Andrey <llianandr94@gmail.com>
2021-12-26 13:50:38 +0200

    Add media width 700 style agencyLevel

--- client/src/components/HomeComponents/AgencyLevel/AgencyLevel.js
+++ client/src/components/HomeComponents/AgencyLevel/AgencyLevel.js
@@ -9,13 +9,16 @@ const AgencyLevel = () => {
   return (
     <section className={styles.section}>
       <h2 className={styles.header}>agency level experience</h2>
-      {articles.map((article,i)=>{
+      <div className={styles.articlesWrapper}>
+        {articles.map((article,i)=>{
         return <article key={i} className={styles.article}>
           <div style={{"background-color":article.iconBg}} className={styles.iconWrapper}><i style={{"color":article.iconColor}} className={`${article.icon} fa-2x`}></i></div>
           <h3 className={styles.capture}>{article.capture}</h3>
           <p  className={styles.discription}>{article.discription}<Link   className={styles.articleLink} to={article.link}>Learn More</Link></p>
         </article>
       })}
+      </div>
+      
       <Link className={styles.btnLink} to='/managed-contests'>learn more about agency services</Link>
     </section>
   );
